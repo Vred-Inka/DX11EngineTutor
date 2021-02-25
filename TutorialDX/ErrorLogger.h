49391a18a1a8341f31558aa94a06bee8e892c2e5
Vredinka <37695796+VredInna@users.noreply.github.com>
2021-02-25 23:02:29 +0200

    Add files via upload

---
+++ TutorialDX/ErrorLogger.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "StringConverter.h"
+#include <Windows.h>
+
+class ErrorLogger
+{
+public:
+    static void Log(std::string message);
+    static void Log(HRESULT hr, std::string message);
+
+private:
+
+};
